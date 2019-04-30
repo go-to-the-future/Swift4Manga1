5702c84cc63f2925da1ddbb99b4a3d4f76ae2100
Kiyotan <appleumasugi.mjd@gmail.com>
2019-04-30 13:52:07 +0900

    Initial Commit

---
+++ Swift4Manga1Tests/Swift4Manga1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Swift4Manga1Tests.swift
+//  Swift4Manga1Tests
+//
+//  Created by Kiyoto Ryuman on 2019/04/30.
+//  Copyright © 2019 Kiyoto Ryuman. All rights reserved.
+//
+
+import XCTest
+@testable import Swift4Manga1
+
+class Swift4Manga1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
